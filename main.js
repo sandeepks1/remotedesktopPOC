fb4644ff72062489722de6360f26bebbf5b07e23
Sandeep KS <Sandeep.KS@Dell.com>
2023-08-23 04:09:08 +0530

    remote

--- main.js
+++ main.js
@@ -1,13 +1,178 @@
-let APP_ID = "5c63aa7f50074ab2996c1b3b5e80d482"
+// let APP_ID = "5c63aa7f50074ab2996c1b3b5e80d482"
 
 
-let peerConnection;
-let localStream;
-let remoteStream;
+// let peerConnection;
+// let localStream;
+// let remoteStream;
 
-let uid = String(Math.floor(Math.random() * 10000))
-let token = null;
-let client;
+// let uid = String(Math.floor(Math.random() * 10000))
+// let token = null;
+// let client;
+
+// let servers = {
+//     iceServers: [{
+//         urls: "stun:stun.relay.metered.ca:80"
+//     }, {
+//         urls: "turn:a.relay.metered.ca:80",
+//         username: "835c5389b98d89346569c234",
+//         credential: "TFEyZVKp00MJOBNJ",
+//     }, {
+//         urls: "turn:a.relay.metered.ca:80?transport=tcp",
+//         username: "835c5389b98d89346569c234",
+//         credential: "TFEyZVKp00MJOBNJ",
+//     }, {
+//         urls: "turn:a.relay.metered.ca:443",
+//         username: "835c5389b98d89346569c234",
+//         credential: "TFEyZVKp00MJOBNJ",
+//     }, {
+//         urls: "turn:a.relay.metered.ca:443?transport=tcp",
+//         username: "835c5389b98d89346569c234",
+//         credential: "TFEyZVKp00MJOBNJ",
+//     }, ],
+// }
+
+
+// let init = async() => {
+//     console.log("init async")
+//     client = await AgoraRTM.createInstance(APP_ID)
+//     await client.login({ uid, token })
+
+//     const channel = client.createChannel('main')
+//     channel.join()
+
+//     channel.on('MemberJoined', handlePeerJoined)
+//     client.on('MessageFromPeer', handleMessageFromPeer)
+
+//     //localStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
+
+//     //document.getElementById('user-1').srcObject = localStream
+// }
+
+// let handlePeerJoined = async(MemberId) => {
+//     console.log("handle perr joined")
+//     console.log('A new peer has joined this room:', MemberId)
+//     createOffer(MemberId)
+// }
+
+// let handleMessageFromPeer = async(message, MemberId) => {
+//     console.log("handleMessageFromPeer")
+//     message = JSON.parse(message.text)
+//     console.log('Message:', message.type)
+
+//     if (message.type === 'offer') {
+//         console.log("offer")
+//             if (!localStream) {
+//                 localStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
+
+//             document.getElementById('user-1').srcObject = localStream
+//         }
+
+//         document.getElementById('offer-sdp').value = JSON.stringify(message.offer)
+//         createAnswer(MemberId)
+//     }
+
+//     if (message.type === 'answer') {
+//         console.log("answer")
+//         document.getElementById('answer-sdp').value = JSON.stringify(message.answer)
+//         addAnswer()
+//     }
+
+//     if (message.type === 'candidate') {
+//         console.log("candidate")
+//         if (peerConnection) {
+//             peerConnection.addIceCandidate(message.candidate)
+//         }
+//     }
+// }
+
+// let createPeerConnection = async(sdpType, MemberId) => {
+//     console.log("peerconnection")
+//     peerConnection = new RTCPeerConnection(servers)
+
+//     remoteStream = new MediaStream()
+//     document.getElementById('user-2').srcObject = remoteStream
+
+//     // localStream.getTracks().forEach((track) => {
+//     //     peerConnection.addTrack(track, localStream)
+//     // })
+
+//     peerConnection.ontrack = async(event) => {
+//         event.streams[0].getTracks().forEach((track) => {
+//             remoteStream.addTrack(track)
+//         })
+//     }
+
+//     peerConnection.onicecandidate = async(event) => {
+//         console.log("on ice")
+//         if (event.candidate) {
+//             document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
+//             client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId)
+//         }
+//     }
+// }
+
+// let createOffer = async(MemberId) => {
+//     console.log("craete offewer")
+//     createPeerConnection('offer-sdp', MemberId)
+
+//     let offer = await peerConnection.createOffer()
+//     await peerConnection.setLocalDescription(offer)
+
+//     document.getElementById('offer-sdp').value = JSON.stringify(offer)
+//     client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, MemberId)
+// }
+
+// let createAnswer = async(MemberId) => {
+//     console.log("create answer")
+//     createPeerConnection('answer-sdp', MemberId)
+
+//     let offer = document.getElementById('offer-sdp').value
+//     if (!offer) return console.log('Retrieve offer from peer first...')
+
+//     offer = JSON.parse(offer)
+//     await peerConnection.setRemoteDescription(offer)
+
+//     let answer = await peerConnection.createAnswer()
+//     await peerConnection.setLocalDescription(answer)
+
+//     document.getElementById('answer-sdp').value = JSON.stringify(answer)
+//     client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'answer': answer }) }, MemberId)
+// }
+
+// let addAnswer = async() => {
+//     console.log("add answer")
+//     let answer = document.getElementById('answer-sdp').value
+//     if (!answer) return console.log('Retrieve answer from peer first...')
+
+//     answer = JSON.parse(answer)
+
+//     if (!peerConnection.currentRemoteDescription) {
+//         peerConnection.setRemoteDescription(answer)
+//     }
+
+// }
+
+// init()
+// var sendChannel = peerConnection.createDataChannel("sendChannel");
+// sendChannel.onmessage = (e) => console.log("message received: " + e.data);
+// sendChannel.onopen = () =>
+//     console.log("Channel opened");
+// // Send a message when the channel is open
+
+// sendChannel.onclose = () => console.log("channel closed");
+
+// function connecttoPC() {
+//     sendChannel.send("heeelooooo")
+
+// }
+// // document.getElementById('create-offer').addEventListener('click', createOffer)
+// // document.getElementById('create-answer').addEventListener('click', createAnswer)
+// // document.getElementById('add-answer').addEventListener('click', addAnswer)
+
+
+
+
+//you can specify a STUN server here
 
 let servers = {
     iceServers: [{
@@ -31,140 +196,33 @@ let servers = {
     }, ],
 }
 
+const localConnection = new RTCPeerConnection(servers)
 
-let init = async() => {
-    console.log("init async")
-    client = await AgoraRTM.createInstance(APP_ID)
-    await client.login({ uid, token })
 
-    const channel = client.createChannel('main')
-    channel.join()
 
-    channel.on('MemberJoined', handlePeerJoined)
-    client.on('MessageFromPeer', handleMessageFromPeer)
+//const localConnection = new RTCPeerConnection()
 
-    //localStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
 
-    //document.getElementById('user-1').srcObject = localStream
+localConnection.onicecandidate = e => {
+    console.log(" NEW ice candidate!! on localconnection reprinting SDP ")
+    console.log(JSON.stringify(localConnection.localDescription))
+    document.getElementById('offer-sdp').value = JSON.stringify(localConnection.localDescription)
 }
 
-let handlePeerJoined = async(MemberId) => {
-    console.log("handle perr joined")
-    console.log('A new peer has joined this room:', MemberId)
-    createOffer(MemberId)
+
+var sendChannel = localConnection.createDataChannel("sendChannel");
+sendChannel.onmessage = e => console.log("messsage received!!!" + e.data)
+sendChannel.onopen = e => console.log("open!!!!");
+sendChannel.onclose = e => console.log("closed!!!!!!");
+
+
+localConnection.createOffer().then(o => localConnection.setLocalDescription(o))
+
+function addAnswer() {
+    answer = document.getElementById('answer-sdp').value
+    localConnection.setRemoteDescription(JSON.parse(answer)).then(a => console.log("done"))
 }
-
-let handleMessageFromPeer = async(message, MemberId) => {
-    console.log("handleMessageFromPeer")
-    message = JSON.parse(message.text)
-    console.log('Message:', message.type)
-
-    if (message.type === 'offer') {
-        console.log("offer")
-            // if (!localStream) {
-            //     localStream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: false });
-
-        //     document.getElementById('user-1').srcObject = localStream
-        // }
-
-        document.getElementById('offer-sdp').value = JSON.stringify(message.offer)
-        createAnswer(MemberId)
-    }
-
-    if (message.type === 'answer') {
-        console.log("answer")
-        document.getElementById('answer-sdp').value = JSON.stringify(message.answer)
-        addAnswer()
-    }
-
-    if (message.type === 'candidate') {
-        console.log("candidate")
-        if (peerConnection) {
-            peerConnection.addIceCandidate(message.candidate)
-        }
-    }
-}
-
-let createPeerConnection = async(sdpType, MemberId) => {
-    console.log("peerconnection")
-    peerConnection = new RTCPeerConnection(servers)
-
-    remoteStream = new MediaStream()
-    document.getElementById('user-2').srcObject = remoteStream
-
-    // localStream.getTracks().forEach((track) => {
-    //     peerConnection.addTrack(track, localStream)
-    // })
-
-    peerConnection.ontrack = async(event) => {
-        event.streams[0].getTracks().forEach((track) => {
-            remoteStream.addTrack(track)
-        })
-    }
-
-    peerConnection.onicecandidate = async(event) => {
-        console.log("on ice")
-        if (event.candidate) {
-            document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
-            client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId)
-        }
-    }
-}
-
-let createOffer = async(MemberId) => {
-    console.log("craete offewer")
-    createPeerConnection('offer-sdp', MemberId)
-
-    let offer = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(offer)
-
-    document.getElementById('offer-sdp').value = JSON.stringify(offer)
-    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, MemberId)
-}
-
-let createAnswer = async(MemberId) => {
-    console.log("create answer")
-    createPeerConnection('answer-sdp', MemberId)
-
-    let offer = document.getElementById('offer-sdp').value
-    if (!offer) return console.log('Retrieve offer from peer first...')
-
-    offer = JSON.parse(offer)
-    await peerConnection.setRemoteDescription(offer)
-
-    let answer = await peerConnection.createAnswer()
-    await peerConnection.setLocalDescription(answer)
-
-    document.getElementById('answer-sdp').value = JSON.stringify(answer)
-    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'answer': answer }) }, MemberId)
-}
-
-let addAnswer = async() => {
-    console.log("add answer")
-    let answer = document.getElementById('answer-sdp').value
-    if (!answer) return console.log('Retrieve answer from peer first...')
-
-    answer = JSON.parse(answer)
-
-    if (!peerConnection.currentRemoteDescription) {
-        peerConnection.setRemoteDescription(answer)
-    }
-
-}
-
-init()
-var sendChannel = peerConnection.createDataChannel("sendChannel");
-sendChannel.onmessage = (e) => console.log("message received: " + e.data);
-sendChannel.onopen = () =>
-    console.log("Channel opened");
-// Send a message when the channel is open
-
-sendChannel.onclose = () => console.log("channel closed");
 
 function connecttoPC() {
-    sendChannel.send("heeelooooo")
-
+    sendChannel.send("connecttodesktop")
 }
-// document.getElementById('create-offer').addEventListener('click', createOffer)
-// document.getElementById('create-answer').addEventListener('click', createAnswer)
-// document.getElementById('add-answer').addEventListener('click', addAnswer)
---
+++ remote.js
@@ -0,0 +1,73 @@
+function craeteAnswer() {
+
+    let servers = {
+        iceServers: [{
+            urls: "stun:stun.relay.metered.ca:80"
+        }, {
+            urls: "turn:a.relay.metered.ca:80",
+            username: "835c5389b98d89346569c234",
+            credential: "TFEyZVKp00MJOBNJ",
+        }, {
+            urls: "turn:a.relay.metered.ca:80?transport=tcp",
+            username: "835c5389b98d89346569c234",
+            credential: "TFEyZVKp00MJOBNJ",
+        }, {
+            urls: "turn:a.relay.metered.ca:443",
+            username: "835c5389b98d89346569c234",
+            credential: "TFEyZVKp00MJOBNJ",
+        }, {
+            urls: "turn:a.relay.metered.ca:443?transport=tcp",
+            username: "835c5389b98d89346569c234",
+            credential: "TFEyZVKp00MJOBNJ",
+        }, ],
+    }
+
+
+    var remoteConnection = new RTCPeerConnection(servers)
+
+
+    offer = document.getElementById('offer-sdp').value
+    remoteConnection.onicecandidate = e => {
+        console.log(" NEW ice candidnat!! on localconnection reprinting SDP ")
+        console.log(JSON.stringify(remoteConnection.localDescription))
+        document.getElementById('answer-sdp').value = JSON.stringify(remoteConnection.localDescription)
+    }
+
+
+    remoteConnection.ondatachannel = e => {
+
+        const receiveChannel = e.channel;
+        receiveChannel.onmessage = e => {
+            console.log("messsage received!!!" + e.data);
+            if ((e.data).toString() == "connecttodesktop") {
+                console.log("share screen")
+
+                navigator.mediaDevices.getDisplayMedia({
+                    video: true
+                }).then(stream => {
+                    const video = document.getElementById('video');
+                    video.srcObject = stream;
+                    video.onloadedmetadata = () => {
+                        video.play();
+                    }
+                    stream.getTracks().forEach(track => remoteConnection.addTrack(track, stream));
+                }).catch(error => {
+                    console.error('Error opening video camera.', error);
+                });
+
+            }
+        }
+        receiveChannel.onopen = e => console.log("open!!!!");
+        receiveChannel.onclose = e => console.log("closed!!!!!!");
+        remoteConnection.channel = receiveChannel;
+
+    }
+
+
+    remoteConnection.setRemoteDescription(JSON.parse(offer)).then(a => console.log("done"))
+
+    //create answer
+    remoteConnection.createAnswer().then(a => remoteConnection.setLocalDescription(a)).then(a =>
+        console.log(JSON.stringify(remoteConnection.localDescription)))
+}
+//create answer
